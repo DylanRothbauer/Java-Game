c2b206e0321a301ba17e6a9fe368ebaf9277fe42
DylanRothbauer <djrothbauer@gmail.com>
2024-11-27 14:25:41 -0600

    Making a Java Game

---
+++ main.java/src/main/App.java
@@ -0,0 +1,21 @@
+package main;
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("JavaGame");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
